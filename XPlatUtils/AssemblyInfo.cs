41d87d905d91216b09506be24ce5c3cfb85832e1
Jonathan Peppers <jonathan.peppers@gmail.com>
2012-10-24 21:40:22 -0500

    Switched to portable library for XPlatUtils library

---
+++ XPlatUtils/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("XPlatUtils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonathan Peppers")]
+[assembly: AssemblyProduct("XPlatUtils")]
+[assembly: AssemblyCopyright("Jonathan Peppers")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.*")]
