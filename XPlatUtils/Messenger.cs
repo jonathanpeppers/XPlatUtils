dec87ac320cef5b7cb7ee27dfe3a0bee843af909
Jonathan Peppers <jonathan.peppers@gmail.com>
2012-10-26 21:26:37 -0500

    Added WeakReferences to Messenger class

--- XPlatUtils/Messenger.cs
+++ XPlatUtils/Messenger.cs
@@ -6,48 +6,70 @@ using System.Linq;
 namespace XPlatUtils {
     /// <summary>
     /// Messenger hub responsible for taking subscriptions/publications and delivering of messages.
-    /// TODO: measure performance impact of weak references and implement
+    /// NOTE: uses WeakReferences, so forgetting to unsubscribe will not cause memory leaks
     /// </summary>
     public sealed class MessengerHub : IMessengerHub {
-        Dictionary<Type, List<Delegate>> messages = new Dictionary<Type, List<Delegate>> ();
+        Dictionary<Type, List<WeakReference>> messages = new Dictionary<Type, List<WeakReference>> ();
 
+        /// <summary>
+        /// Subscribes an action to a message of a particular type
+        /// </summary>
         public void Subscribe<TMessage> (Action<TMessage> deliveryAction) where TMessage : Message
         {
             if (deliveryAction == null) {
                 throw new ArgumentNullException ("deliveryAction");
             }
 
-            List<Delegate> list;
+            List<WeakReference> list;
             if (!messages.TryGetValue (typeof (TMessage), out list)) {
                 messages [typeof (TMessage)] =
-                        list = new List<Delegate> ();
+                        list = new List<WeakReference> ();
             }
-            list.Add (deliveryAction);
+            list.Add (new WeakReference(deliveryAction, false));
         }
-
+        
+        /// <summary>
+        /// Unsubscribes from messages of a particular type
+        /// </summary>
         public void Unsubscribe<TMessage> (Action<TMessage> deliveryAction) where TMessage : Message
         {
             if (deliveryAction == null) {
                 throw new ArgumentNullException ("deliveryAction");
             }
 
-            List<Delegate> list;
+            List<WeakReference> list;
             if (messages.TryGetValue (typeof (TMessage), out list)) {
-                list.Remove (deliveryAction);
+                WeakReference subscription;
+                for (int i = 0; i < list.Count; i++) {
+                    subscription = list [i];
+                    if ((Action<TMessage>)subscription.Target == deliveryAction) {
+                        list.RemoveAt (i);
+                        break;
+                    }
+                }
             }
         }
 
+        /// <summary>
+        /// Publishes a message, which is sent out to all subscribers
+        /// </summary>
         public void Publish<TMessage> (TMessage message) where TMessage : Message
         {
             if (message == null) {
                 throw new ArgumentNullException ("message");
             }
 
-            List<Delegate> list;
+            List<WeakReference> list;
             if (messages.TryGetValue (typeof (TMessage), out list)) {
                 //Call ToArray() in case someone unsubscribes during the iteration
-                foreach (Action<TMessage> subscription in list.ToArray ()) {
-                    subscription (message);
+                foreach (WeakReference subscription in list.ToArray ()) {
+                    if (subscription.IsAlive) {
+                        Action<TMessage> action = subscription.Target as Action<TMessage>;
+                        if (action != null)
+                            action (message);
+                    } else {
+                        list.Remove (subscription);
+                    }
                 }
             }
         }
