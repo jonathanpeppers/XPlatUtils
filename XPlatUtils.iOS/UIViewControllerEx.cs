1f997b4e549f2e4d447f16afe114237573d8fea4
Jonathan Peppers <jonathan.peppers@gmail.com>
2013-03-16 15:27:49 -0500

    Setup more delegate classes

--- XPlatUtils.iOS/UIViewControllerEx.cs
+++ XPlatUtils.iOS/UIViewControllerEx.cs
@@ -8,7 +8,7 @@ namespace XPlatUtils.iOS
 {
 	/// <summary>
 	/// An extension on UIViewController including most strongly typed overrides for use with WeakDelegate on iOS
-	/// * Including: UITableViewSource, UIScrollViewDelegate
+	/// * Including: UITableViewSource, UIScrollViewDelegate, UITextFieldDelegate, UITextViewDelegate
 	/// </summary>
 	public class UIViewControllerEx : UIViewController
 	{
@@ -72,38 +72,8 @@ namespace XPlatUtils.iOS
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
         
-		[Export("scrollViewDidEndDecelerating:")]
-		public virtual void DecelerationEnded (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewWillBeginDecelerating:")]
-        public virtual void DecelerationStarted (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
 		[Export("tableView:didEndEditingRowAtIndexPath:")]
 		public virtual void DidEndEditing (UITableView tableView, NSIndexPath indexPath)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewDidZoom:")]
-        public virtual void DidZoom (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewDidEndDragging:willDecelerate:")]
-        public virtual void DraggingEnded (UIScrollView scrollView, bool willDecelerate)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewWillBeginDragging:")]
-        public virtual void DraggingStarted (UIScrollView scrollView)
 		{
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
@@ -216,24 +186,6 @@ namespace XPlatUtils.iOS
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
         
-		[Export("scrollViewDidEndScrollingAnimation:")]
-        public virtual void ScrollAnimationEnded (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewDidScroll:")]
-        public virtual void Scrolled (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("scrollViewDidScrollToTop:")]
-        public virtual void ScrolledToTop (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
 		[Export("tableView:sectionForSectionIndexTitle:atIndex:")]
 		public virtual int SectionFor (UITableView tableView, string title, int atIndex)
 		{
@@ -258,12 +210,6 @@ namespace XPlatUtils.iOS
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
         
-		[Export("scrollViewShouldScrollToTop:")]
-        public virtual bool ShouldScrollToTop (UIScrollView scrollView)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
 		[Export("tableView:shouldShowMenuForRowAtIndexPath:")]
 		public virtual bool ShouldShowMenu (UITableView tableView, NSIndexPath rowAtindexPath)
 		{
@@ -284,12 +230,6 @@ namespace XPlatUtils.iOS
         
 		[Export("tableView:titleForHeaderInSection:")]
 		public virtual string TitleForHeader (UITableView tableView, int section)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
-		[Export("viewForZoomingInScrollView:")]
-        public virtual UIView ViewForZoomingInScrollView (UIScrollView scrollView)
 		{
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
@@ -324,29 +264,255 @@ namespace XPlatUtils.iOS
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
         
-		[Export("scrollViewWillEndDragging:withVelocity:targetContentOffset:")]
-        public virtual void WillEndDragging (UIScrollView scrollView, PointF velocity, ref PointF targetContentOffset)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
-        
 		[Export("tableView:willSelectRowAtIndexPath:")]
 		public virtual NSIndexPath WillSelectRow (UITableView tableView, NSIndexPath indexPath)
 		{
 			throw new You_Should_Not_Call_base_In_This_Method ();
 		}
         
-		[Export("scrollViewDidEndZooming:withView:atScale:")]
-        public virtual void ZoomingEnded (UIScrollView scrollView, UIView withView, float atScale)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();
-		}
+        #endregion
         
-		[Export("scrollViewWillBeginZooming:withView:")]
-        public virtual void ZoomingStarted (UIScrollView scrollView, UIView view)
-		{
-			throw new You_Should_Not_Call_base_In_This_Method ();   
-		}
+        #region UIScrollViewDelegate
+        
+        [Export("scrollViewDidEndDecelerating:")]
+        public virtual void DecelerationEnded(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewWillBeginDecelerating:")]
+        public virtual void DecelerationStarted(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidZoom:")]
+        public virtual void DidZoom(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidEndDragging:willDecelerate:")]
+        public virtual void DraggingEnded(UIScrollView scrollView, bool willDecelerate)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewWillBeginDragging:")]
+        public virtual void DraggingStarted(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidEndScrollingAnimation:")]
+        public virtual void ScrollAnimationEnded(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidScroll:")]
+        public virtual void Scrolled(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidScrollToTop:")]
+        public virtual void ScrolledToTop(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewShouldScrollToTop:")]
+        public virtual bool ShouldScrollToTop(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("viewForZoomingInScrollView:")]
+        public virtual UIView ViewForZoomingInScrollView(UIScrollView scrollView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewWillEndDragging:withVelocity:targetContentOffset:")]
+        public virtual void WillEndDragging(UIScrollView scrollView, PointF velocity, ref PointF targetContentOffset)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewDidEndZooming:withView:atScale:")]
+        public virtual void ZoomingEnded(UIScrollView scrollView, UIView withView, float atScale)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("scrollViewWillBeginZooming:withView:")]
+        public virtual void ZoomingStarted(UIScrollView scrollView, UIView view)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        #endregion
+        
+        #region UITextFieldDelegate
+        
+        [Export("textFieldDidEndEditing:")]
+        public virtual void EditingEnded(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textFieldDidBeginEditing:")]
+        public virtual void EditingStarted(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textFieldShouldBeginEditing:")]
+        public virtual bool ShouldBeginEditing(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textField:shouldChangeCharactersInRange:replacementString:")]
+        public virtual bool ShouldChangeCharacters(UITextField textField, NSRange range, string replacementString)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textFieldShouldClear:")]
+        public virtual bool ShouldClear(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textFieldShouldEndEditing:")]
+        public virtual bool ShouldEndEditing(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textFieldShouldReturn:")]
+        public virtual bool ShouldReturn(UITextField textField)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        #endregion
+        
+        #region UITextViewDelegate
+        
+        [Export("textViewDidChange:")]
+        public virtual void Changed(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textViewDidEndEditing:")]
+        public virtual void EditingEnded(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textViewDidBeginEditing:")]
+        public virtual void EditingStarted(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textViewDidChangeSelection:")]
+        public virtual void SelectionChanged(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textViewShouldBeginEditing:")]
+        public virtual bool ShouldBeginEditing(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textView:shouldChangeTextInRange:replacementText:")]
+        public virtual bool ShouldChangeText(UITextView textView, NSRange range, string text)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("textViewShouldEndEditing:")]
+        public virtual bool ShouldEndEditing(UITextView textView)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        #endregion
+        
+        #region UISearchBarDelegate
+        
+        [Export("searchBarBookmarkButtonClicked:")]
+        public virtual void BookmarkButtonClicked(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarCancelButtonClicked:")]
+        public virtual void CancelButtonClicked(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarResultsListButtonClicked:")]
+        public virtual void ListButtonClicked(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarTextDidBeginEditing:")]
+        public virtual void OnEditingStarted(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarTextDidEndEditing:")]
+        public virtual void OnEditingStopped(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarSearchButtonClicked:")]
+        public virtual void SearchButtonClicked(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBar:selectedScopeButtonIndexDidChange:")]
+        public virtual void SelectedScopeButtonIndexChanged(UISearchBar searchBar, int selectedScope)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarShouldBeginEditing:")]
+        public virtual bool ShouldBeginEditing(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBar:shouldChangeTextInRange:replacementText:")]
+        public virtual bool ShouldChangeTextInRange(UISearchBar searchBar, NSRange range, string text)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBarShouldEndEditing:")]
+        public virtual bool ShouldEndEditing(UISearchBar searchBar)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
+        
+        [Export("searchBar:textDidChange:")]
+        public virtual void TextChanged(UISearchBar searchBar, string searchText)
+        {
+            throw new You_Should_Not_Call_base_In_This_Method();
+        }
         
         #endregion
 	}
