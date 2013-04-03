2047e3ec6f5ddef933d300530b5f8c1b7850b55a
Jonathan Peppers <jonathan.peppers@gmail.com>
2013-04-03 09:44:38 -0500

    Added "scoping" to ServiceContainer
    Really useful on Android

--- XPlatUtils/Messenger.cs
+++ XPlatUtils/Messenger.cs
@@ -3,47 +3,55 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-namespace XPlatUtils {
+namespace XPlatUtils
+{
     /// <summary>
     /// Messenger hub responsible for taking subscriptions/publications and delivering of messages.
     /// NOTE: uses WeakReferences, so forgetting to unsubscribe will not cause memory leaks
     /// </summary>
-    public sealed class Messenger {
-        Dictionary<Type, List<WeakReference>> messages = new Dictionary<Type, List<WeakReference>> ();
+    public sealed class Messenger
+    {
+        Dictionary<Type, List<WeakReference>> messages = new Dictionary<Type, List<WeakReference>>();
 
         /// <summary>
         /// Subscribes an action to a message of a particular type
         /// </summary>
-        public void Subscribe<TMessage> (Action<TMessage> deliveryAction) where TMessage : Message
+        public void Subscribe<TMessage>(Action<TMessage> deliveryAction) where TMessage : Message
         {
-            if (deliveryAction == null) {
-                throw new ArgumentNullException ("deliveryAction");
+            if (deliveryAction == null)
+            {
+                throw new ArgumentNullException("deliveryAction");
             }
 
             List<WeakReference> list;
-            if (!messages.TryGetValue (typeof (TMessage), out list)) {
-                messages [typeof (TMessage)] =
-                        list = new List<WeakReference> ();
+            if (!messages.TryGetValue(typeof(TMessage), out list))
+            {
+                messages[typeof(TMessage)] =
+                        list = new List<WeakReference>();
             }
-            list.Add (new WeakReference(deliveryAction, false));
+            list.Add(new WeakReference(deliveryAction, false));
         }
-        
+
         /// <summary>
         /// Unsubscribes from messages of a particular type
         /// </summary>
-        public void Unsubscribe<TMessage> (Action<TMessage> deliveryAction) where TMessage : Message
+        public void Unsubscribe<TMessage>(Action<TMessage> deliveryAction) where TMessage : Message
         {
-            if (deliveryAction == null) {
-                throw new ArgumentNullException ("deliveryAction");
+            if (deliveryAction == null)
+            {
+                throw new ArgumentNullException("deliveryAction");
             }
 
             List<WeakReference> list;
-            if (messages.TryGetValue (typeof (TMessage), out list)) {
+            if (messages.TryGetValue(typeof(TMessage), out list))
+            {
                 WeakReference subscription;
-                for (int i = 0; i < list.Count; i++) {
-                    subscription = list [i];
-                    if ((Action<TMessage>)subscription.Target == deliveryAction) {
-                        list.RemoveAt (i);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    subscription = list[i];
+                    if ((Action<TMessage>)subscription.Target == deliveryAction)
+                    {
+                        list.RemoveAt(i);
                         break;
                     }
                 }
@@ -53,22 +61,28 @@ namespace XPlatUtils {
         /// <summary>
         /// Publishes a message, which is sent out to all subscribers
         /// </summary>
-        public void Publish<TMessage> (TMessage message) where TMessage : Message
+        public void Publish<TMessage>(TMessage message) where TMessage : Message
         {
-            if (message == null) {
-                throw new ArgumentNullException ("message");
+            if (message == null)
+            {
+                throw new ArgumentNullException("message");
             }
 
             List<WeakReference> list;
-            if (messages.TryGetValue (typeof (TMessage), out list)) {
+            if (messages.TryGetValue(typeof(TMessage), out list))
+            {
                 //Call ToArray() in case someone unsubscribes during the iteration
-                foreach (WeakReference subscription in list.ToArray ()) {
-                    if (subscription.IsAlive) {
+                foreach (WeakReference subscription in list.ToArray())
+                {
+                    if (subscription.IsAlive)
+                    {
                         Action<TMessage> action = subscription.Target as Action<TMessage>;
                         if (action != null)
-                            action (message);
-                    } else {
-                        list.Remove (subscription);
+                            action(message);
+                    }
+                    else
+                    {
+                        list.Remove(subscription);
                     }
                 }
             }
@@ -78,24 +92,26 @@ namespace XPlatUtils {
     /// <summary>
     /// Base class for messages that provides weak refrence storage of the sender
     /// </summary>
-    public abstract class Message {
+    public abstract class Message
+    {
         /// <summary>
         /// Gets the original sender of the message
         /// </summary>
-        public object Sender {
+        public object Sender
+        {
             get;
             private set;
         }
-        
+
         /// <summary>
         /// Initializes a new instance of the MessageBase class.
         /// </summary>
         /// <param name="sender">Message sender (usually "this")</param>
-        public Message (object sender)
+        public Message(object sender)
         {
             if (sender == null)
-                throw new ArgumentNullException ("sender");
-            
+                throw new ArgumentNullException("sender");
+
             Sender = sender;
         }
     }
--- XPlatUtils/ServiceContainer.cs
+++ XPlatUtils/ServiceContainer.cs
@@ -3,80 +3,132 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace XPlatUtils {
+namespace XPlatUtils
+{
     /// <summary>
     /// A simple service container implementation, singleton only
     /// </summary>
-    public static class ServiceContainer {
-        static readonly Dictionary<Type, Lazy<object>> services = new Dictionary<Type, Lazy<object>> ();
+    public static class ServiceContainer
+    {
+        static readonly Dictionary<Type, Lazy<object>> services = new Dictionary<Type, Lazy<object>>();
+        static readonly Stack<Dictionary<Type, object>> scopedServices = new Stack<Dictionary<Type, object>>();
 
         /// <summary>
         /// Register the specified service with an instance
         /// </summary>
-        public static void Register<T> (T service)
+        public static void Register<T>(T service)
         {
-            services [typeof (T)] = new Lazy<object> (() => service);
+            services[typeof(T)] = new Lazy<object>(() => service);
         }
 
         /// <summary>
         /// Register the specified service for a class with a default constructor
         /// </summary>
-        public static void Register<T> () where T : new ()
+        public static void Register<T>() where T : new()
         {
-            services [typeof (T)] = new Lazy<object> (() => new T ());
+            services[typeof(T)] = new Lazy<object>(() => new T());
         }
 
         /// <summary>
         /// Register the specified service with a callback to be invoked when requested
         /// </summary>
-        public static void Register<T> (Func<T> function)
+        public static void Register<T>(Func<T> function)
         {
-            services [typeof (T)] = new Lazy<object> (() => function ());
+            services[typeof(T)] = new Lazy<object>(() => function());
         }
 
         /// <summary>
         /// Register the specified service with an instance
         /// </summary>
-        public static void Register (Type type, object service)
+        public static void Register(Type type, object service)
         {
-            services [type] = new Lazy<object> (() => service);
+            services[type] = new Lazy<object>(() => service);
         }
 
         /// <summary>
         /// Register the specified service with a callback to be invoked when requested
         /// </summary>
-        public static void Register (Type type, Func<object> function)
+        public static void Register(Type type, Func<object> function)
         {
-            services [type] = new Lazy<object> (function);
+            services[type] = new Lazy<object>(function);
         }
 
         /// <summary>
-        /// Resolves the type, throwing an exception if not found
+        /// Register the specified service with an instance that is scoped
         /// </summary>
-        public static T Resolve<T> ()
+        public static void RegisterScoped<T>(T service)
         {
-            return (T)Resolve (typeof (T));
-        }
-
-        /// <summary>
-        /// Resolves the type, throwing an exception if not found
-        /// </summary>
-        public static object Resolve (Type type)
-        {
-            Lazy<object> service;
-            if (services.TryGetValue (type, out service)) {
-                return service.Value;
-            } else {
-                throw new KeyNotFoundException (string.Format ("Service not found for type '{0}'", type));
+            Dictionary<Type, object> services;
+            if (scopedServices.Count == 0)
+            {
+                services = new Dictionary<Type, object>();
+                scopedServices.Push(services);
             }
+            else
+            {
+                services = scopedServices.Peek();
+            }
+
+            services[typeof(T)] = service;
+        }
+
+        /// <summary>
+        /// Resolves the type, throwing an exception if not found
+        /// </summary>
+        public static T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        /// <summary>
+        /// Resolves the type, throwing an exception if not found
+        /// </summary>
+        public static object Resolve(Type type)
+        {
+            //Scoped services
+            if (scopedServices.Count > 0)
+            {
+                var services = scopedServices.Peek();
+
+                object service;
+                if (services.TryGetValue(type, out service))
+                {
+                    return service;
+                }
+            }
+
+            //Non-scoped services
+            {
+                Lazy<object> service;
+                if (services.TryGetValue(type, out service))
+                {
+                    return service.Value;
+                }
+                else
+                {
+                    throw new KeyNotFoundException(string.Format("Service not found for type '{0}'", type));
+                }
+            }
+        }
+
+        public static void AddScope()
+        {
+            scopedServices.Push(new Dictionary<Type, object>());
+        }
+
+        public static void RemoveScope()
+        {
+            if (scopedServices.Count > 0)
+                scopedServices.Pop();
         }
 
         /// <summary>
         /// Mainly for testing, clears the entire container
         /// </summary>
-        public static void Clear ()
+        public static void Clear()
         {
-            services.Clear ();
+            services.Clear();
+            scopedServices.Clear();
         }
     }
 }
